ed16b8d96b28d20b2e3c8db1db59c900b8249b4f
Vinay K <vinaylamba9@gmail.com>
2023-10-14 02:09:10 -0400

    added title for skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -23,41 +23,75 @@ const Skills = () => {
       <p>Skills</p>
       <div className='skills__icons'>
         <div className='skills__icons--inner'>
-          <img src={HTML5} alt='HTML' />
-          <img src={CSS} alt='CSS' />
-          <img src={JS} alt='JavaScript' />
-          <img src={ReactIcon} alt='React' />
-          <img src={TS} alt='TypeScript' />
-          <img src={NodeJs} alt='NodeJs' />
+          <img src={HTML5} alt='HTML' title='HTML' />
+          <img src={CSS} alt='CSS' title='CSS' />
+          <img src={JS} alt='JavaScript' title='JavaScript' />
+          <img src={ReactIcon} alt='React' title='React' />
+          <img src={TS} alt='TypeScript' title='TypeScript' />
+          <img src={NodeJs} alt='NodeJs' title='NodeJs' />
 
-          {/* <img src="" alt="Express" /> */}
-          <img src={Java} alt='Java' />
-          <img src={C} alt='C++' />
-          <img src={Sass} alt='SASS' />
-          <img src={GraphQL} alt='GraphQL' />
-          <img src={MySQL} alt='MySQL' />
-          <img src={PostGreSQL} alt='Posgresql' />
-          <img src={Redis} alt='Redis' />
-          <img src={Docker} alt='Docker' />
-          <img src={AWS} alt='AWS' />
+          {/*  <img src="" alt="Express" title="Express" />  */}
+          <img src={Java} alt='Java' title='Java' />
+          <img src={C} alt='C++' title='C++' />
+          <img src={Sass} alt='SASS' title='SASS' />
+          <img src={GraphQL} alt='GraphQL' title='GraphQL' />
+          <img src={MySQL} alt='MySQL' title='MySQL' />
+          <img src={PostGreSQL} alt='Posgresql' title='Posgresql' />
+          <img src={Redis} alt='Redis' title='Redis' />
+          <img src={Docker} alt='Docker' title='Docker' />
+          <img src={AWS} alt='AWS' title='AWS' />
 
-          <img aria-label='duplicate' src={HTML5} alt='HTML' />
-          <img aria-label='duplicate' src={CSS} alt='CSS' />
-          <img aria-label='duplicate' src={JS} alt='JavaScript' />
-          <img aria-label='duplicate' src={ReactIcon} alt='React' />
-          <img aria-label='duplicate' src={TS} alt='TypeScript' />
-          <img aria-label='duplicate' src={NodeJs} alt='NodeJs' />
-
-          {/* <img aria-label='duplicate' src="" alt="Express" /> */}
-          <img aria-label='duplicate' src={Java} alt='Java' />
-          <img aria-label='duplicate' src={C} alt='C++' />
-          <img aria-label='duplicate' src={Sass} alt='SASS' />
-          <img aria-label='duplicate' src={GraphQL} alt='GraphQL' />
-          <img aria-label='duplicate' src={MySQL} alt='MySQL' />
-          <img aria-label='duplicate' src={PostGreSQL} alt='Posgresql' />
-          <img aria-label='duplicate' src={Redis} alt='Redis' />
-          <img aria-label='duplicate' src={Docker} alt='Docker' />
-          <img aria-label='duplicate' src={AWS} alt='AWS' />
+          <img aria-label='duplicate' src={HTML5} alt='HTML' title='HTML' />
+          <img aria-label='duplicate' src={CSS} alt='CSS' title='CSS' />
+          <img
+            aria-label='duplicate'
+            src={JS}
+            alt='JavaScript'
+            title='JavaScript'
+          />
+          <img
+            aria-label='duplicate'
+            src={ReactIcon}
+            alt='React'
+            title='React'
+          />
+          <img
+            aria-label='duplicate'
+            src={TS}
+            alt='TypeScript'
+            title='TypeScript'
+          />
+          <img
+            aria-label='duplicate'
+            src={NodeJs}
+            alt='NodeJs'
+            title='NodeJs'
+          />
+          {/* <img aria-label='duplicate' src="" alt="Express" title="Express" />  */}
+          <img aria-label='duplicate' src={Java} alt='Java' title='Java' />
+          <img aria-label='duplicate' src={C} alt='C++' title='C++' />
+          <img aria-label='duplicate' src={Sass} alt='SASS' title='SASS' />
+          <img
+            aria-label='duplicate'
+            src={GraphQL}
+            alt='GraphQL'
+            title='GraphQL'
+          />
+          <img aria-label='duplicate' src={MySQL} alt='MySQL' title='MySQL' />
+          <img
+            aria-label='duplicate'
+            src={PostGreSQL}
+            alt='Posgresql'
+            title='Posgresql'
+          />
+          <img aria-label='duplicate' src={Redis} alt='Redis' title='Redis' />
+          <img
+            aria-label='duplicate'
+            src={Docker}
+            alt='Docker'
+            title='Docker'
+          />
+          <img aria-label='duplicate' src={AWS} alt='AWS' title='AWS' />
         </div>
       </div>
       <div className='technologies'>
